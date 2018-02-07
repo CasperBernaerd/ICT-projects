54cea86f41f511674fb914dc8e8f1f6369d1c740
CasperBernaerd <35937945+CasperBernaerd@users.noreply.github.com>
2018-02-07 20:19:30 +0100

    Basic template

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+	<head>
+	<title> Website </title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/screen.css">
+</head>
+<body>
+	<header>
+		<h1 class="logo">Logo</h1>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="downloads.php">Downloads</a></li>
+				<li><a href="forum.php">Forum</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</nav>
+	<div class="clear"></div>
+	</header>
+	<div id="container">
+		<aside>
+			<div class="widget">
+				<h2>Widget header</h2>
+				<div class="inner">
+					Widget contents
+				</div>
+			</div>
+		</aside>
+		
+		<h1>Home</h1>
+		<p>Just a template.</p>
+	</div>
+	<footer>
+		&copy; Casper 2018
+	</footer>
+</body>
+</html>
+		
