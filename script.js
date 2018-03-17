c22a2f78eb131fc97fdede7a99d338c492c728e5
CasperBernaerd <35937945+CasperBernaerd@users.noreply.github.com>
2018-03-17 21:14:05 +0100

    bugfixes javascript

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var navSize;
 function openNav() {
 	document.getElementById("sidenav").style.width = "250px";
+	if (window.innerWidth >= 700 ) {
+		document.body.style.marginLeft = "250px";
+		document.body.style.transition = "all 0.5s";
+	}
 	navSize = 250;
 }
 function closeNav() {
@@ -13,12 +17,15 @@ function toggleNav(){
 	return openNav();
 }	
 
-
 window.onresize = function(){	//voor sidenav , zodat op ipad de sidenav de tekst mee verschuift en mobiel het menu over de tekst schuift
-	if (window.innerWidth >= 700 ) {
+	if (window.innerWidth <= 700 && navSize == 250) { // mobiel
+		document.body.style.marginLeft = "0";
+		document.body.style.transition = "all 0s";
+	} else if (window.innerWidth >= 700 && window.innerWidth <= 1100 && navSize == 250) {	//ipad
 		document.body.style.marginLeft = "250px";
 		document.body.style.transition = "all 0.5s";
-	} else if (window.innerWidth <= 700) {
+	} else if (window.innerWidth >= 1100 && navSize == 250 ) {	//desktop
+		closeNav();
 		document.body.style.marginLeft = "0";
 		document.body.style.transition = "all 0s";
 	}
