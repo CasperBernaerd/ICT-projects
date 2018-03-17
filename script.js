d4f4a2de00e7b8ad28f774052f310e56dfa5feee
CasperBernaerd <35937945+CasperBernaerd@users.noreply.github.com>
2018-03-17 20:56:27 +0100

    nog meer javascript goodies
    Goede menu overgang in ipad naar mobiel en andersom

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 var navSize;
 function openNav() {
-		document.getElementById("sidenav").style.width = "250px";
-		if (window.innerWidth >= 700) { //voor sidenav , zodat op ipad de sidenav de tekst mee verschuift en mobiel het menu over de tekst schuift
-			document.body.style.marginLeft = "250px";
-			document.body.style.transition = "all 0.5s";
-		}
-		
-		navSize = 250;
-	}
+	document.getElementById("sidenav").style.width = "250px";
+	navSize = 250;
+}
 function closeNav() {
-		document.getElementById("sidenav").style.width = "0";
-		document.body.style.marginLeft = "0";
-		navSize = 0;
-	}
+	document.getElementById("sidenav").style.width = "0";
+	document.body.style.marginLeft = "0";
+	navSize = 0;
+}
 function toggleNav(){
-			if (navSize === 250) {return closeNav();}
-			return openNav();
-		}	
-		
+	if (navSize === 250) {return closeNav();}
+	return openNav();
+}	
+
+
+window.onresize = function(){	//voor sidenav , zodat op ipad de sidenav de tekst mee verschuift en mobiel het menu over de tekst schuift
+	if (window.innerWidth >= 700 ) {
+		document.body.style.marginLeft = "250px";
+		document.body.style.transition = "all 0.5s";
+	} else if (window.innerWidth <= 700) {
+		document.body.style.marginLeft = "0";
+		document.body.style.transition = "all 0s";
+	}
+};
 		
 window.onscroll = function() {myFunction()};
 
