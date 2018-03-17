21cd01c8d9c258e7ca9d3a74048f5efea7aaf35b
CasperBernaerd <35937945+CasperBernaerd@users.noreply.github.com>
2018-03-17 20:38:07 +0100

    menu + javascript

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+var navSize;
 function openNav() {
 		document.getElementById("sidenav").style.width = "250px";
+		if (window.innerWidth >= 700) { //voor sidenav , zodat op ipad de sidenav de tekst mee verschuift en mobiel het menu over de tekst schuift
+			document.body.style.marginLeft = "250px";
+			document.body.style.transition = "all 0.5s";
+		}
+		
+		navSize = 250;
 	}
 function closeNav() {
 		document.getElementById("sidenav").style.width = "0";
+		document.body.style.marginLeft = "0";
+		navSize = 0;
 	}
+function toggleNav(){
+			if (navSize === 250) {return closeNav();}
+			return openNav();
+		}	
+		
+		
+window.onscroll = function() {myFunction()};
+
+		var navbar = document.getElementById("nav");
+		var sticky = navbar.offsetTop;
+
+		function myFunction() {
+			if (window.pageYOffset >= sticky) {
+				navbar.classList.add("sticky")
+				navbar.style.backgroundColor = "rgba(100,100,100,0.6)";
+				} 
+			else {
+			navbar.classList.remove("sticky"); 
+			navbar.style.backgroundColor = "rgba(255,255,255,0.2)";
+			}
+		}
